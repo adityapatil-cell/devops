1ab8748f80614072bade526ced48de1edbe8c774
adityapatil <adityapatil1998@gmail.com>
2020-03-07 12:11:24 +0530

    jar

---
+++ SecondAttempt/src/main/java/com/cg/Hello.java
@@ -0,0 +1,10 @@
+package com.cg;
+
+public class Hello {
+	
+	public String sayHello() {
+		return "Hello eorld";
+		
+	}
+
+}
---
+++ git/src/git/test.java
@@ -0,0 +1,29 @@
+package git;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class test {
+
+	public class TestHello{
+		private Hello h1;
+		
+		@Before
+		public void init() {
+			h1=new Hello();
+		}
+		
+		@After
+		public void clean() {
+			h1=null;
+		}
+		
+		@Test
+		public void tetstsayHello() {
+assertEquals("hello world",h1.sayHello());
+	}
+}
+}
